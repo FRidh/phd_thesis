21997139f8863a84b77e7c5f3bddf45b7f7f6341
Frederik Rietdijk <fridh@fridh.nl>
2017-08-16 19:18:58 +0200

    update

--- nix/announcement/default.nix
+++ nix/announcement/default.nix
@@ -10,17 +10,18 @@ with stdenv.lib;
 
 let
   filename = "main-for-defence-announcement";
-  XELATEX="xelatex -halt-on-error";
+#   LATEX="xelatex -halt-on-error";
+  LATEX="pdflatex";
 in stdenv.mkDerivation {
   name = "thesis-announcement.pdf";
   src = thesis-source;
 
-  sourceRoot = "${thesis-source.name}/report2";
+  sourceRoot = "${thesis-source.name}/report";
 
   buildInputs = [ latex biber ghostscript ];
 
   buildPhase = ''
-    ${XELATEX} ${filename}.tex #&& biber ${filename}.bcf && biber ${filename}.bcf && ${XELATEX} ${filename}.tex
+    ${LATEX} ${filename}.tex
   '';
 
   installPhase = ''
